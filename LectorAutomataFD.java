1d3036065513628b8ba7b045563c34b166be88c5
Saúl Cañedo Castillo <saul.canedocast@hotmail.com>
2020-10-18 13:44:53 -0700

    first commit

---
+++ LectorAutomataFD.java
@@ -0,0 +1,43 @@
+package lectorAutomatasFD;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class LectorAutomataFD {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					LectorAutomataFD window = new LectorAutomataFD();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public LectorAutomataFD() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
